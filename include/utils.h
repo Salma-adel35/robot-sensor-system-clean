21ba7f656720c18b5188e206d3b31cf663570e62
salma adel <salma.adel2015maaly@gmail.com>
2025-10-23 20:44:58 +0200

    Format utils.h with consistent spacing

--- include/utils.h
+++ include/utils.h
@@ -5,17 +5,17 @@
 #include <random>
 
 namespace Utils {
-    // 1. توليد ID فريد
+    
     int generateID();
 
-    // 2. توليد رقم عشري عشوائي
+   
     double randomDouble(double min, double max);
 
-    // 3. محاكاة الفرصة العشوائية (للفشل)
+    
     bool chance(double probability);
 
-    // 4. الحصول على التاريخ والوقت الحالي
+    
     std::string currentDateTime();
 }
 
-#endif // UTILS_H
+#endif 
